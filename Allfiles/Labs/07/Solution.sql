db7b658d3e8e9322ec7d850986a2d53ad5f13482
Graeme Malcolm <gmalc@microsoft.com>
2022-06-01 17:50:52 -0400

    Fixed typo in comment

--- Allfiles/Labs/07/Solution.sql
+++ Allfiles/Labs/07/Solution.sql
@@ -53,13 +53,13 @@ GROUP BY d.FiscalYear, t.SalesTerritoryRegion
 ORDER BY d.FiscalYear;
 
 
--- Approximate number of sales orders perfiscal year by territory
+-- Approximate number of sales orders per fiscal year by territory
 SELECT  d.FiscalYear,
         t. SalesTerritoryRegion AS SalesTerritory,
-        APPROX_COUNT_DISTINCT(s.SalesOrderNumber) AS Orders
+        APPROX_COUNT_DISTINCT(s.SalesOrderNumber) AS ApproxOrders
 FROM FactResellerSales AS s
 JOIN DimDate AS d ON s.OrderDateKey = d.DateKey
 JOIN DimEmployee AS e ON s.EmployeeKey = e.EmployeeKey
 JOIN DimSalesTerritory AS t ON e.SalesTerritoryKey = t.SalesTerritoryKey
 GROUP BY d.FiscalYear, t.SalesTerritoryRegion
-ORDER BY d.FiscalYear, Orders;
+ORDER BY d.FiscalYear, ApproxOrders;

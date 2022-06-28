6f88cacf8c6ecde409f45f52998674265ba9e114
James Herring <49248205+TheJamesHerring@users.noreply.github.com>
2022-06-28 13:00:23 -0400

    Dataverse requires eastus, eastus2, or eastuseuap

--- Allfiles/Labs/08/setup.ps1
+++ Allfiles/Labs/08/setup.ps1
@@ -83,7 +83,7 @@ $resourceGroupName = "dp000-$suffix"
 Write-Host "Finding an available region. This may take several minutes...";
 $delay = 0, 30, 60, 90, 120 | Get-Random
 Start-Sleep -Seconds $delay # random delay to stagger requests from multi-student classes
-$preferred_list = "australiaeast","centralus","southcentralus","eastus2","northeurope","southeastasia","uksouth","westeurope","westus","westus2"
+$preferred_list = "eastus","eastus2"
 $locations = Get-AzLocation | Where-Object {
     $_.Providers -contains "Microsoft.Synapse" -and
     $_.Providers -contains "Microsoft.Sql" -and

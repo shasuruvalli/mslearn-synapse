107186a5ab660fb2024a8caa53bf874921a4eca4
Graeme Malcolm <gmalc@microsoft.com>
2022-07-01 09:22:35 -0400

    Added setup files for purview lab (10)

---
+++ Allfiles/Labs/10/setup.sql
@@ -0,0 +1,13 @@
+﻿SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Products](
+	[ProductID] [int] NOT NULL,
+	[ProductName] [nvarchar(50)] NOT NULL,
+	[Category] [nvarchar(50)] NOT NULL,
+	[ListPrice] [money] NULL
+)
+GO
+
+
